a159b04f976c38cce86367914a9c01f2b0166225
Russell Klein <fuzzyklein2@gmail.com>
2021-09-18 17:24:35 -0500

    Change the names to protect the innocent.

--- hw/globals.py
+++
@@ -1,6 +0,0 @@
-""" Define some global variables to be accessed from different modules. """
-
-from pathlib import Path
-
-BASEDIR = Path(__file__).parent.parent
-PROGRAM = BASEDIR.stem.split('-')[0]
--- hw/hw.py
+++
@@ -1,9 +0,0 @@
-from program import Program
-
-class HelloWorld(Program):
-    def process_file(self, p):
-        super().process_file(p)
-
-    def run(self):
-        super().run()
-        print('Hello world')
